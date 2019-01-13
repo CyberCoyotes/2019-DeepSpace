c496691f027817051f058fa17ea6c3b0f2721ffb
Connor Williams <minisolarclown@gmail.com>
2019-01-12 19:49:00 -0500

    Added a talon and added vendordeps to .gitignore

--- 2019DeepSpaceFRC/src/main/java/frc/robot/Robot.java
+++ 2019DeepSpaceFRC/src/main/java/frc/robot/Robot.java
@@ -1,10 +1,14 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
-
+  
+  WPI_TalonSRX left1 = new WPI_TalonSRX(1);
+  
   @Override
   public void robotInit() {
   }
