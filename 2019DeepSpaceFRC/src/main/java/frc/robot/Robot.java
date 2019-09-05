22e0b94d2b5ddb16e914aa69097cbb8bfc0e625c
Road Runner <cybercoyotes@gmail.com>
2019-09-05 16:11:24 -0400

    test

--- 2019DeepSpaceFRC/src/main/java/frc/robot/Robot.java
+++ 2019DeepSpaceFRC/src/main/java/frc/robot/Robot.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -40,13 +39,10 @@ public class Robot extends TimedRobot {
   DoubleSolenoid.Value in = DoubleSolenoid.Value.kReverse;
   DoubleSolenoid.Value out = DoubleSolenoid.Value.kForward;
   DoubleSolenoid shifter = new DoubleSolenoid(3, 4);
-  DoubleSolenoid frontLift = new DoubleSolenoid(1, 6);
-  DoubleSolenoid backLift = new DoubleSolenoid(5, 2);
-  Solenoid puncher = new Solenoid(7);
+  DoubleSolenoid leftFlipper = new DoubleSolenoid(5, 2);
+  DoubleSolenoid rightFlipper = new DoubleSolenoid (6, 1);
+  Solenoid puncher = new Solenoid(0);
 
-  Servo leftServo = new Servo(0);
-  Servo rightServo = new Servo(1);
-  
   Joystick driver = new Joystick(0);//Joystick for the driver
   Joystick manip = new Joystick(1);
   AHRS navx = new AHRS(Port.kMXP);//NavX
@@ -55,7 +51,6 @@ public class Robot extends TimedRobot {
   CTREEncoder elevatorEnc = new CTREEncoder(elevator1, false);
   PressureSensor pressureSensor = new PressureSensor(3);
   Encoder wristEnc = new Encoder(0, 1, false, EncodingType.k2X);
-  DigitalInput topSwitch = new DigitalInput(2);
 
   double liftSetPoint = 0;
   double wristSetPoint = 94;
@@ -69,15 +64,15 @@ public class Robot extends TimedRobot {
   double previous = 0;
   final double hatch1 = 0;//TO DO
   final double hatch2 = 26896;
-  final double port1 = 13000;
+  final double port1 = 5464;
   final double port2 = 30000;
 
   long servoTime = Long.MAX_VALUE;
   int lastPOVState = -1;
   boolean down = true;
   boolean wristDown = true;
-  boolean lift20Toggle = false;
   boolean wasAuton = false;
+  boolean servoToggle = false;
 
   @Override
   public void robotInit() {
@@ -117,8 +112,6 @@ public class Robot extends TimedRobot {
       wristSetPoint = 85;
       elevatorEnc.reset();
       liftSetPoint = elevatorEnc.get();
-      rightServo.set(0);
-      leftServo.set(1);
       wasAuton = false;
     }
   }
@@ -134,12 +127,9 @@ public class Robot extends TimedRobot {
       liftSetPoint = hatch2;
       limelight.setPipeline(1);
     }
-    if((Math.abs(y) >= 0.15 || Math.abs(rot) >= 0.15) && !driver.getRawButton(1) && !driver.getRawButton(2)) {
+    if((Math.abs(y) >= 0.15 || Math.abs(rot) >= 0.15) && !driver.getRawButton(1)) {
       mainDrive.arcadeDrive(y, rot);
     } else if(driver.getRawButton(1) && limelight.hasValidTarget() && elevatorEnc.get() > 23000) {
-      double rSpeed = limelight.getX()*turnKP;
-      mainDrive.arcadeDrive(y, rSpeed);
-    } else if(driver.getRawButton(2) && limelight.hasValidTarget()) {
       double rSpeed = limelight.getX()*turnKP;
       mainDrive.arcadeDrive(y, rSpeed);
     } else {
@@ -150,8 +140,11 @@ public class Robot extends TimedRobot {
       liftSetPoint = previous;
     }
 
-    shifter.set(out);
-
+    if(driver.getRawButton(2)) {
+      shifter.set(out);
+    } else {
+      shifter.set(in);
+    }
 
     double liftSpeed = manip.getRawAxis(1); //Get the manual lift speed
     if(liftSpeed < 0) { //If the manual speed is negative...
@@ -161,30 +154,22 @@ public class Robot extends TimedRobot {
     }
     if(driver.getRawButtonPressed(12)) {  //If the driver pressed button 12...
       liftSetPoint = hatch1;  //Set the base height to hatch1
-      if(lift20Toggle) {  //If the hook offset is true...
-        liftSetPoint+=liftHatchOffset;  //Add the offset
-      }
     }
     if(driver.getRawButtonPressed(11)) {  //If the driver pressed button 12...
       liftSetPoint = port1;  //Set the base height to port1
-      lift20Toggle = false;
-    }
+      }
     if(driver.getRawButtonPressed(10)) {  //If the driver presses button 10...
       liftSetPoint = hatch2;  //Set the base height to hatch2
-      if(lift20Toggle) {  //If the hook offset is true...
-        liftSetPoint+=liftHatchOffset;  //Add the offsett
-      }
     }
     if(driver.getRawButtonPressed(9)) {  //If the driver presses button 10...
       liftSetPoint = port2;  //Set the base height to port2
-      lift20Toggle = false;
     }
-    if(Math.abs(liftSpeed) >= 0.05 && !(!topSwitch.get() && liftSpeed > 0) && elevatorEnc.get() > 750) { //If the manip wants to manual control the elevator...
+    if(Math.abs(liftSpeed) >= 0.05 && elevatorEnc.get() > 250) { //If the manip wants to manual control the elevator...
       elevator.set(liftSpeed);  //Set the elevator speed
       liftSetPoint = elevatorEnc.get(); //Set the PID setpoint to the current reading
     } else {  //Else (if the PID is activated)
-      if(elevatorEnc.get() < 750) {
-        liftSetPoint = 1250;
+      if(elevatorEnc.get() < 250) {
+        liftSetPoint = 750;
       }
       liftSpeed = (liftSetPoint - elevatorEnc.get()) * liftKP;  //Calculate the lift speed based on error
       if(liftSpeed > 0.4) { //Limit the down speed
@@ -194,29 +179,31 @@ public class Robot extends TimedRobot {
       }
       elevator.set(liftSpeed);  //Set the elevator
     }
-    if(driver.getRawButtonPressed(5)) { //If button 5 is pressed...
-      lift20Toggle=!lift20Toggle; //Switch the lift hook toggle
-      if(lift20Toggle) {  //If the toggle is on...
-        liftSetPoint+=liftHatchOffset;  //Add the toggle
-      } else {  //If the toggle is off...
-        liftSetPoint-=liftHatchOffset;  //Remove the toggle
-      }
-    }
-    if(driver.getRawButtonPressed(3)) { //If button 3 is pressed...
+    if(driver.getRawButtonPressed(5)) { //If button 3 is pressed...
       liftSetPoint+=liftOffset; //Add 2 inches
     }
-    if(driver.getRawButtonReleased(3)) {  //If button 3 is released...
+    if(driver.getRawButtonReleased(5)) {  //If button 3 is released...
       liftSetPoint-=liftOffset; //Remove 2 inches
     }
-    if(driver.getRawButtonPressed(4)) { //If button 4 is pressed...
+    if(driver.getRawButtonPressed(6)) { //If button 4 is pressed...
       liftSetPoint-=liftOffset; //Remove 2 inches
     }
-    if(driver.getRawButtonReleased(4)) {  //If button 4 is released
+    if(driver.getRawButtonReleased(6)) {  //If button 4 is released
       liftSetPoint+=liftOffset; //Add 2 inches
+    }
+    if(driver.getRawButton(3)) {
+      leftFlipper.set(out);
+    } else {
+      leftFlipper.set(in); 
+    }
+    if(driver.getRawButton(4)) {
+      rightFlipper.set(out);
+    } else {
+      rightFlipper.set(in);
     }
 
     double wristSpeed = manip.getRawAxis(5);  //Get the manual wrist speed
-    wristSpeed = wristSpeed > 0 ? wristSpeed*=0.25 : wristSpeed; //Limit the down speed
+    wristSpeed = wristSpeed > 0 ? wristSpeed*=0.35 : wristSpeed; //Limit the down speed
     double wristPosition = (-94.0/91.0)*wristEnc.get()+94;  //Calculate the current angle
     if(Math.abs(wristSpeed) >= 0.05) {  //If the manip wants to manually move the wrist...
       wrist.set(wristSpeed);  //Set wrist 1
@@ -226,49 +213,80 @@ public class Robot extends TimedRobot {
       wristSpeed = (wristSetPoint - wristPosition) * wristKP; //Calculate the speed based off of the error
       wrist.set(wristSpeed);  //Set wrist1
       wrist2.set(wristSpeed); //Set wrist2
-
       SmartDashboard.putNumber("Wrist Speed", wristSpeed);  //Publish the speed
     }
-    if(manip.getRawButtonPressed(4)) {
-      wristSetPoint = 90;
-    }
     if(manip.getRawButtonPressed(2)) {
-      wristSetPoint = 45;//Was 30
+      wristSetPoint = 30;//Was 30
     }
 
+    if(manip.getRawButton(1)) {
+      wristSetPoint = 90;
+    }
+
+    /*****
     int currentPOVState = manip.getPOV();//Get the manip POV
     if(currentPOVState > lastPOVState) {//If the current POV angle is different than the old one...
-      if(currentPOVState == 0) {//if the POV is up...
-        servoTime = System.currentTimeMillis();//Record the time
-        down = true;//Active the servo down boolean
-        //rightServo.set(0);//Set the right servo to up
-      } else if(currentPOVState == 180) {//if the POV is down...
+      if(currentPOVState == 180) {//if the POV is down...
         servoTime = System.currentTimeMillis();//Record the time
         down = false;//Deactivate the servo down boolean
-        //rightServo.set(1);//Set the right servo to down
-        intake.set(-0.4);
+        //wristSetPoint = 55;
       }
     }
-    if(!down && System.currentTimeMillis() - servoTime >= 400) {//If it has been 300ms since the POV was pushed down...
-      //leftServo.set(0);//Set the left servo to down
-      intake.set(0);
+    if(!down && System.currentTimeMillis() - servoTime > 750) {
+      intake.set(-0.2);
+    }
+    if(!down && System.currentTimeMillis() - servoTime >= 1500) {//If it has been 300ms since the POV was pushed down...
+      //intake.set(0);
+      //wristSetPoint-=5;/////
       puncher.set(true);
     }
-    if(!down && System.currentTimeMillis() - servoTime >= 850) {//If it has been 600ms since the POV was pushed down...
-      //puncher.set(true);//Activate the punchers
+    if(!down && System.currentTimeMillis() - servoTime >= 2000) {//If it has been 600ms since the POV was pushed down...
       puncher.set(false);
+      intake.set(0);
       down = true;
     }
     lastPOVState = currentPOVState;//Reset the last POV value
+*/
 
+
+/*
+int currentPOVState = manip.getPOV();//Get the manip POV
+    if(currentPOVState > lastPOVState) {//If the current POV angle is different than the old one...
+      if(currentPOVState == 180) {//if the POV is down...
+        servoTime = System.currentTimeMillis();//Record the time
+        down = false;//Deactivate the servo down boolean
+        intake.set(-0.25);
+      }
+    }
+    if(!down && System.currentTimeMillis() - servoTime >= 500) {//If it has been 300ms since the POV was pushed down...
+      //intake.set(0);
+      //wristSetPoint-=5;/////
+      puncher.set(true);
+    }
+    if(!down && System.currentTimeMillis() - servoTime >= 1250) {//If it has been 600ms since the POV was pushed down...
+      puncher.set(false);
+      intake.set(0);
+      down = true;
+    }
+    lastPOVState = currentPOVState;//Reset the last POV value*/
+
+
+    
     if (manip.getRawAxis(2) >= 0.25 && down) {//If left trigger is pressed...
       intake.set(-0.6);//Move the intake out
     } else if (manip.getRawAxis(3) >= 0.25 && down) {//If right trigger is pressed...
       intake.set(0.6); //Move the intake motor in.
-    } else if(down) {//If nothing is pressed...
-      intake.set(0);//Set the intake to stop
-    }
-
+    } else if(manip.getRawButton(5)) {
+      intake.set(-0.3);
+    } else {
+      intake.set(0);
+    }     
+    
+    puncher.set(manip.getRawButton(6));
+      //else if(down) {//If nothing is pressed...
+      //intake.set(0);//Set the intake to stop
+    //}
+    
     read();//Read from sensors
   }
 
